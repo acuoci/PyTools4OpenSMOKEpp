4282050a727e80c22221096e1832dc2ac63b36e6
Alberto Cuoci <alberto.cuoci@gmail.com>
2021-07-04 14:02:51 +0200

    Updated examples

--- examples/RemoveReactions.py
+++ examples/RemoveReactions.py
@@ -26,8 +26,8 @@ sys.path.insert(0, '../src')
 # Import PyTools classes
 from KineticMechanism import KineticMechanism
 from OpenSMOKEppXMLFile import OpenSMOKEppXMLFile
+from PolimiSootModule import *
 from Utilities import *
-
 
 
 # Official CRECK2012 Soot BINJOnly (no heavy fuels version)
@@ -35,6 +35,14 @@ folder_github = "C:\\Users\\acuoci\\OneDrive - Politecnico di Milano\\My Project
 kin_xml_folder_name=folder_github + "CRECK_DiscreteSectionalModel_v2012\\CRECK_2012_Soot_OnlyBINJ_NoHeavyFuels\\kinetics-CRECK_2012_SootOnlyBINJ-SP-AGG\\"
 kinetics = KineticMechanism(kin_xml_folder_name + "kinetics.xml")
 kinetics.ReadKinetics(kin_xml_folder_name + "reaction_names.xml")
+
+
+# Create groups
+kinetics.AddGroupOfSpecies('PAH12', 'PAHs with 1/2 aromatic rings', DefaultPAH12())
+kinetics.AddGroupOfSpecies('PAH34', 'PAHs with 3/4 aromatic rings', DefaultPAH34())
+kinetics.AddGroupOfSpecies('PAHLP', 'PAHs with more than 4 aromatic rings (molecular and radical)', DefaultPAHLP(kinetics.species))
+kinetics.AddGroupOfSpecies('SP', 'BIN sections corresponding to spherical particles (molecular and radical)', DefaultSP(kinetics.species))
+kinetics.AddGroupOfSpecies('AGG', 'BIN sections corresponding to aggregates (molecular and radical)', DefaultAGG(kinetics.species))
 
 
 # Remove reactions containing a subset of species
